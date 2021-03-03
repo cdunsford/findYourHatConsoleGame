8d81cbdf1d5fbe5f86af742b08a93655ab3ec4b6
Christopher Dunsford <cdunsford@gmail.com>
2021-03-02 23:57:14 -0500

    add logic to move around on the field

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const pathCharacter = "*";
 class Field {
   constructor(field) {
     this._field = field;
+    this.currentLocation = {
+      x: 0,
+      y: 0,
+    };
   }
   print() {
     let fieldAsString = "";
@@ -24,14 +28,88 @@ class Field {
       fieldAsString = "";
     }
   }
+  move(direction) {
+    switch (direction) {
+      case "d":
+        if (this.currentLocation.x + 1 >= this._field.length) {
+          console.log("Error: index out of bounds");
+          break;
+        }
+
+        this.currentLocation.x += 1;
+        this._field[this.currentLocation.x][
+          this.currentLocation.y
+        ] = pathCharacter;
+
+        break;
+      case "u":
+        if (this.currentLocation.x == 0) {
+          console.log("Error: index out of bounds");
+          break;
+        }
+
+        this.currentLocation.x -= 1;
+
+        this._field[this.currentLocation.x][
+          this.currentLocation.y
+        ] = pathCharacter;
+        break;
+      case "l":
+        if (this.currentLocation.y == 0) {
+          console.log("Error: index out of bounds");
+          break;
+        }
+
+        this.currentLocation.y -= 1;
+
+        this._field[this.currentLocation.x][
+          this.currentLocation.y
+        ] = pathCharacter;
+        break;
+      case "r":
+        if (
+          this.currentLocation.y + 1 >=
+          this._field[this.currentLocation.x].length
+        ) {
+          console.log("Error: index out of bounds");
+          break;
+        }
+
+        this.currentLocation.y += 1;
+        this._field[this.currentLocation.x][
+          this.currentLocation.y
+        ] = pathCharacter;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 const myField = new Field([
   ["*", "░", "O"],
   ["░", "O", "░"],
   ["░", "^", "░"],
+  ["░", "░", "░"],
 ]);
 //console.log(myField._field[2][1]);
 myField.print();
-const direction = prompt("Which direction do you want to go?");
-console.log("You chose " + direction);
+
+let direction = "";
+
+do {
+  //direction = prompt("Which direction do you want to go? Or press q to quit: ");
+
+  direction = "d";
+  myField.move(direction);
+  myField.print();
+
+  direction = "l";
+  myField.move(direction);
+  myField.print();
+
+  direction = "r";
+  myField.move(direction);
+} while (direction != "q");
+//let direction = "d";
